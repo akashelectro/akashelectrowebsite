d5cb388865ec1939305fa30824ce87c4ac6244fd
Samarth Viswanath <32961053+lonewolf1005@users.noreply.github.com>
2025-04-30 19:44:46 +0530

    Updated timer

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("May 01, 2025 00:00:00").getTime();
+        var finalDate =  new Date("May 31, 2025 00:00:00").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)
